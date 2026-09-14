24c61e34efb74e614a1169e7c2e78bcf4eea62aa
Sergoanatolu <xalker83@gmail.com>
2026-09-14 20:15:17 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/crypto-traders-app/',
+  base: '/Crypto-trader/',
   plugins: [react()],
 })
